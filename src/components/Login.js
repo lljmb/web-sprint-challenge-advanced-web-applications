9feff0ac530152487e49a90b9302cd208e61d784
lljmb <brown.jessm@icloud.com>
2021-01-29 12:20:37 -0500

    built out form, axios auth and private route

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  // 2. Add whatever state nessiary for form functioning.
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  })
+  const [errors, setErrors] = useState({error: 'Username or Password not valid.'})
+  const { push } = useHistory();
+
+  
+  
+  
+  const changeHandler = e => {
+    e.preventDefault();
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
+
+ 
+
+  const submit = e => {
+    e.preventDefault();
+    // 4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
+    if(user.username !== 'Lambda School' || user.password !== 'i<3Lambd4' ) {
+      return {errors}
+    } else {
+      // 5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
+      // make a post request to retrieve a token from the api
+      axios.post('http://localhost:5000/api/login', user)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        // when you have handled the token, navigate to the BubblePage route
+        push('/bubbles')
+
+      })
+      .catch(err => {
+        console.log('login error: ', err)
+      })
+    }
+  }
+
 
   return (
     <>
@@ -11,15 +52,34 @@ const Login = () => {
         Welcome to the Bubble App!
         <p>Build a login page here</p>
       </h1>
+      {/* 1. Build a form containing a username and password field. */}
+      <form onSubmit={submit}>
+      {/* 3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEST "username" and "password" RESPECTIVELY. */}
+        <label for='username' test-id='username'>Username</label>
+        <input
+        name='username'
+        value={user.username}
+        type='text'
+        onChange={changeHandler}
+        placeholder='enter your username'
+        />
+        <label for='password' test-id='password'>Password</label>
+        <input
+        name='password'
+        value={user.password}
+        type='password'
+        onChange={changeHandler}
+        placeholder='enter your password'
+        />
+        <button>Login</button>
+      </form>
     </>
   );
 };
 
 export default Login;
 
-//Task List:
-//1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE THAT FORM INPUTS INCLUDE THE LABEL TEST "username" and "password" RESPECTIVELY.
-//4. If either the username or password is not displaied display EXACTLY the following words: Username or Password not valid.
-//5. If the username / password is equal to Lambda School / i<3Lambd4, save that token to localStorage.
+
+
+
+
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,29 @@
-
-
-
-
-
-
-
+// necessary imports
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
 
 //Task List:
 //1. Build a PrivateRoute component that redirects if user is not logged in
+const PrivateRoute = ({component: Component, ...rest}) => {
+    return(
+        // render the entire route with the rest of its components
+        <Route {...rest}
+        render={(props) => {
+            // see if token exists
+            if(localStorage.getItem('token')){
+                // if it does, return the component passed into the app with all of its props
+                return <Component {...props} />
+            } else {
+                // if the token does not exist, redirect to the login page
+                return <Redirect to='/login'/>
+            }
+        }
+        }/>
+    )
+}
+
+
+
+
+
